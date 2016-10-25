f7cbb06382a6291d921989c1913319d08cff62da
nandy5 <nandhanandy92@gmail.com>
2016-10-25 23:25:24 +0530

    slider working but have to do many changes

---
+++ app/src/main/java/com/sparrownet/sparrow_net/PrefManager.java
@@ -0,0 +1,40 @@
+package com.sparrownet.sparrow_net;
+
+/**
+ * Created by nandy on 23-Oct-16.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
+
+
